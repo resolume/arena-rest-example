a48993cb82f332154064e388e79cbe8b4b23233d
Martijn Otto <martijn@resolume.com>
2021-02-11 14:02:30 +0100

    Fix the reactjs source path for prod builds and use same host/port

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import './index.css';
 
 // ========================================
 
+function get_option(production, development, fallback) {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
+        return production;
+    } else if (development) {
+        return development;
+    } else {
+        return fallback;
+    }
+}
+
+const host = get_option(location.hostname, process.env.REACT_APP_HOST, '127.0.0.1');
+const port = parseInt(get_option(location.port, process.env.REACT_APP_PORT, 8080), 10);
+
 ReactDOM.render(
-    <Composition
-        host={process.env.REACT_APP_HOST || "127.0.0.1"}
-        port={parseInt(process.env.REACT_APP_PORT, 10) || 8080}
-    />,
+    <Composition host={host} port={port} />,
     document.getElementById('grid')
 );
