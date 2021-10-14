cf8a95df118c9a3e3844c5e8199a74e93e08f42c
Edwin de Koning <edwin@resolume.com>
2021-10-14 13:02:11 +0200

    fixed background color of transport

--- src/timeline.js
+++ src/timeline.js
@@ -24,67 +24,69 @@ function Timeline(props) {
 
     let controls = (
     
-        <div className="timeline">
+        <div className="timeline con">
             <div className="title" onClick={() => setExpanded(!expanded)}>
                 <span className={`arrow ${expanded ? 'down' : 'right'}`}></span>
                 Transport
             </div>
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(props.transport.position.id)}>Position</span>
-                <Parameter
-                    name="Position"
-                    parameter={props.transport.position}
-                    key={props.transport.position.id}
-                    id={props.transport.position.id}
-                />
+            <div className="content">
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(props.transport.position.id)}>Position</span>
+                    <Parameter
+                        name="Position"
+                        parameter={props.transport.position}
+                        key={props.transport.position.id}
+                        id={props.transport.position.id}
+                    />
+                </div>
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(behaviour.playdirection.id)}>Direction</span>
+                    <Parameter
+                        name="Direction"
+                        parameter={behaviour.playdirection}
+                        key={behaviour.playdirection.id}
+                        id={behaviour.playdirection.id}
+                    />
+                </div>
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(behaviour.playmode.id)}>Play Mode</span>
+                    <Parameter
+                        name="Play Mode"
+                        parameter={behaviour.playmode}
+                        key={behaviour.playmode.id}
+                        id={behaviour.playmode.id}
+                    />
+                </div>
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(behaviour.duration.id)}>Duration</span>
+                    <Parameter
+                        name="Duration"
+                        parameter={behaviour.duration}
+                        key={behaviour.duration.id}
+                        id={behaviour.duration.id}
+                    />
+                </div>            
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(behaviour.speed.id)}>Speed</span>
+                    <Parameter
+                        name="Speed"
+                        parameter={behaviour.speed}
+                        key={behaviour.speed.id}
+                        id={behaviour.speed.id}
+                    />
+                </div>             
+                {behaviour.beatloop &&
+                <div>
+                    <span className="label" onDoubleClick={() => handle_reset(behaviour.beatloop.id)}>Beatloop</span>
+                    <Parameter
+                        name="Beatloop"
+                        parameter={behaviour.beatloop}
+                        key={behaviour.beatloop.id}
+                        id={behaviour.beatloop.id}
+                    />
+                </div>            
+                }
             </div>
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(behaviour.playdirection.id)}>Direction</span>
-                <Parameter
-                    name="Direction"
-                    parameter={behaviour.playdirection}
-                    key={behaviour.playdirection.id}
-                    id={behaviour.playdirection.id}
-                />
-            </div>
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(behaviour.playmode.id)}>Play Mode</span>
-                <Parameter
-                    name="Play Mode"
-                    parameter={behaviour.playmode}
-                    key={behaviour.playmode.id}
-                    id={behaviour.playmode.id}
-                />
-            </div>
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(behaviour.duration.id)}>Duration</span>
-                <Parameter
-                    name="Duration"
-                    parameter={behaviour.duration}
-                    key={behaviour.duration.id}
-                    id={behaviour.duration.id}
-                />
-            </div>            
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(behaviour.speed.id)}>Speed</span>
-                <Parameter
-                    name="Speed"
-                    parameter={behaviour.speed}
-                    key={behaviour.speed.id}
-                    id={behaviour.speed.id}
-                />
-            </div>             
-            {behaviour.beatloop &&
-            <div>
-                <span className="label" onDoubleClick={() => handle_reset(behaviour.beatloop.id)}>Beatloop</span>
-                <Parameter
-                    name="Beatloop"
-                    parameter={behaviour.beatloop}
-                    key={behaviour.beatloop.id}
-                    id={behaviour.beatloop.id}
-                />
-            </div>
-            }
         </div>
     );
 
