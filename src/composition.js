3c0f10d04083b698ac13015600820a54e1b5cc09
Martijn Otto <martijn@resolume.com>
2023-07-19 16:24:42 +0200

    Fix index numbers layers with default names

--- src/composition.js
+++ src/composition.js
@@ -32,19 +32,18 @@ function Composition() {
         />
     );
 
-    const clips = [];
-
-    const layers_and_groups = (() => {
+    const { clips, layers_and_groups } = (() => {
         // first get an iterator on both the layers and groups
-        const layer_iter = array_reverse_iterator(context.composition.layers);
-        const group_iter = array_reverse_iterator(context.composition.layergroups);
+        const layer_iter = context.composition.layers[Symbol.iterator]();
+        const group_iter = context.composition.layergroups[Symbol.iterator]();
 
         // load the first value from both
         let layer = layer_iter.next().value;
         let group = group_iter.next().value;
 
-        // this will hold all the resulting layers
-        const result = [];
+        // this will hold all the resulting clips layers
+        const clip_rows = [];
+        const layers_and_groups = [];
 
         // we need to keep track of layer and group indices (necessary
         // so we can translate layer names if they are set to default)
@@ -59,7 +58,7 @@ function Composition() {
             // skip all the layers (since they are already in the group)
             if (!layer || (layer && group && layer.id === group.layers[group.layers.length - 1].id)) {
                 // add layer group to result
-                result.push(
+                layers_and_groups.push(
                     <LayerGroup
                         id={group.id}
                         key={group.id}
@@ -72,57 +71,49 @@ function Composition() {
                     />
                 );
 
-                let column_index = 0;
-
-                // a layer group comes with its own columns
-                for (const column of group.columns) {
-                    clips.push(
-                        <Column
-                            id={column.id}
-                            key={column.id}
-                            index={column_index}
-                            name={column.name}
-                            connected={column.connected}
-                        />
-                    )
-
-                    column_index++;
-                }
-
                 // skip layers already in group
                 for (let i = 0; i < group.layers.length; i++) {
                     // add all the clips
-                    for (const clip of layer.clips) {
-                        clips.push(
-                                <Clip
-                                    id={clip.id}
-                                    key={clip.id}
-                                    thumbnail={clip.thumbnail}
-                                    name={clip.name}
-                                    selected={clip.selected}
-                                    connected={clip.connected}
-                                    dashboard={clip.dashboard}
-                                    audio={clip.audio}
-                                    video={clip.video}
-                                    beatsnap={clip.beatsnap}
-                                    transporttype={clip.transporttype}
-                                    target={clip.target}
-                                    triggerstyle={clip.triggerstyle}
-                                    faderstart={clip.faderstart}
-                                    ignorecolumntrigger={clip.ignorecolumntrigger}
-                                    transport={clip.transport}
-                                />
-                        );
-                    }
+                    clip_rows.push(layer.clips.map(clip =>
+                        <Clip
+                            id={clip.id}
+                            key={clip.id}
+                            thumbnail={clip.thumbnail}
+                            name={clip.name}
+                            selected={clip.selected}
+                            connected={clip.connected}
+                            dashboard={clip.dashboard}
+                            audio={clip.audio}
+                            video={clip.video}
+                            beatsnap={clip.beatsnap}
+                            transporttype={clip.transporttype}
+                            target={clip.target}
+                            triggerstyle={clip.triggerstyle}
+                            faderstart={clip.faderstart}
+                            ignorecolumntrigger={clip.ignorecolumntrigger}
+                            transport={clip.transport}
+                        />
+                    ));
 
                     layer = layer_iter.next().value;
                 }
+
+                // add layer group columns
+                clip_rows.push(group.columns.map((column, index) =>
+                    <Column
+                        id={column.id}
+                        key={column.id}
+                        index={index}
+                        name={column.name}
+                        connected={column.connected}
+                    />
+                ));
 
                 // move to next group
                 group_index++;
                 group = group_iter.next().value;
             } else {
-                result.push(
+                layers_and_groups.push(
                     <Layer
                         id={layer.id}
                         key={layer.id}
@@ -145,28 +136,26 @@ function Composition() {
                 )
 
                 // add all the clips
-                for (const clip of layer.clips) {
-                    clips.push(
-                            <Clip
-                                id={clip.id}
-                                key={clip.id}
-                                thumbnail={clip.thumbnail}
-                                name={clip.name}
-                                selected={clip.selected}
-                                connected={clip.connected}
-                                dashboard={clip.dashboard}
-                                audio={clip.audio}
-                                video={clip.video}
-                                beatsnap={clip.beatsnap}
-                                transporttype={clip.transporttype}
-                                target={clip.target}
-                                triggerstyle={clip.triggerstyle}
-                                faderstart={clip.faderstart}
-                                ignorecolumntrigger={clip.ignorecolumntrigger}
-                                transport={clip.transport}
-                            />
-                    );
-                }
+                clip_rows.push(layer.clips.map(clip =>
+                    <Clip
+                        id={clip.id}
+                        key={clip.id}
+                        thumbnail={clip.thumbnail}
+                        name={clip.name}
+                        selected={clip.selected}
+                        connected={clip.connected}
+                        dashboard={clip.dashboard}
+                        audio={clip.audio}
+                        video={clip.video}
+                        beatsnap={clip.beatsnap}
+                        transporttype={clip.transporttype}
+                        target={clip.target}
+                        triggerstyle={clip.triggerstyle}
+                        faderstart={clip.faderstart}
+                        ignorecolumntrigger={clip.ignorecolumntrigger}
+                        transport={clip.transport}
+                    />
+                ))
 
                 // move to next layer
                 layer_index++;
@@ -174,7 +163,14 @@ function Composition() {
             }
         }
 
-        return result;
+        // reverse and flatten clips
+        clip_rows.reverse();
+        const clips = Array.prototype.concat.apply([], clip_rows);
+
+        // reverse layers
+        layers_and_groups.reverse();
+
+        return { clips, layers_and_groups };
     })();
 
     const decks = context.composition.decks.map((deck) =>
