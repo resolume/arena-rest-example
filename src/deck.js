2c4196833bceba8dbb939e049b817e49108511ce
Martijn Otto <martijn@resolume.com>
2023-09-04 16:31:29 +0200

    Add context menu for decks

--- src/deck.js
+++ src/deck.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ResolumeContext } from './resolume_provider'
 import ParameterMonitor from './parameter_monitor.js'
+import ContextMenu from './context_menu.js';
 import PropTypes from 'prop-types'
 
 /**
@@ -10,13 +11,25 @@ function Deck({ id, name, selected }) {
     const context   = useContext(ResolumeContext);
     const select    = () => context.action('trigger', `/composition/decks/by-id/${id}/select`);
 
+    const menu_options = {
+        'New':                      { action: () => context.post('/composition/decks/add')                                      },
+        'Insert Before':            { action: () => context.post('/composition/decks/add', `/composition/decks/by-id/${id}`)    },
+        'Duplicate':                { action: () => context.post(`/composition/decks/by-id/${id}/duplicate`)                    },
+        'Remove':                   { action: () => context.remove(`/composition/decks/by-id/${id}`)                            },
+    };
+
     return (
         <div
             className={`deck ${selected ? 'selected' : ''}`}
             onClick={select}
         >
             <ParameterMonitor.Single parameter={name} render={name => (
-                <span>{name.value}</span>
+                <ContextMenu
+                    name={name.value}
+                    options={menu_options}
+                >
+                    <span>{name.value}</span>
+                </ContextMenu>
             )} />
         </div>
     );
